bd9949bac0fc2ce91cd342577b0d6abaac0406da
77eshike <77eshike@gmail.com>
2025-07-21 06:14:08 +0800

    添加 ESLint Flat Config 配置文件

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,69 +1,22 @@
-import eslintPluginReact from 'eslint-plugin-react';
-import eslintPluginReactHooks from 'eslint-plugin-react-hooks';
-import eslintPluginJsxA11y from 'eslint-plugin-jsx-a11y';
-import eslintPluginImport from 'eslint-plugin-import';
 import eslintPluginUnusedImports from 'eslint-plugin-unused-imports';
-import eslintPluginTailwind from 'eslint-plugin-tailwindcss';
-import typescriptEslint from '@typescript-eslint/eslint-plugin';
-import parser from '@typescript-eslint/parser';
+import tseslint from 'typescript-eslint';
 
-export default [
+export default tseslint.config(
   {
-    files: ['**/*.{ts,tsx}'],
-    languageOptions: {
-      parser,
-      parserOptions: {
-        sourceType: 'module',
-        ecmaVersion: 2022,
-        ecmaFeatures: {
-          jsx: true,
-        },
-        project: './tsconfig.json',
-      },
-    },
+    files: ['src/**/*.ts', 'src/**/*.tsx'],
     plugins: {
-      '@typescript-eslint': typescriptEslint,
-      react: eslintPluginReact,
-      'react-hooks': eslintPluginReactHooks,
-      'jsx-a11y': eslintPluginJsxA11y,
-      import: eslintPluginImport,
       'unused-imports': eslintPluginUnusedImports,
-      tailwindcss: eslintPluginTailwind
     },
     rules: {
-      '@typescript-eslint/no-explicit-any': 'warn',
-      'react/jsx-uses-react': 'off',
-      'react/react-in-jsx-scope': 'off',
-      'react-hooks/rules-of-hooks': 'error',
-      'react-hooks/exhaustive-deps': 'warn',
-      'jsx-a11y/label-has-associated-control': 'warn',
-      'import/order': [
-        'warn',
-        {
-          'newlines-between': 'always',
-          distinctGroup: true,
-          warnOnUnassignedImports: false
-        }
-      ],
-      'unused-imports/no-unused-imports': 'warn',
+      'no-console': 'warn',
+      'no-debugger': 'error',
+      'require-await': 'warn',
+      '@typescript-eslint/no-unused-vars': 'off',
+      'unused-imports/no-unused-imports': 'error',
       'unused-imports/no-unused-vars': [
         'warn',
-        {
-          vars: 'all',
-          varsIgnorePattern: '^_',
-          args: 'after-used',
-          argsIgnorePattern: '^_'
-        }
-      ],
-      'tailwindcss/classnames-order': 'warn',
-      'tailwindcss/no-custom-classname': 'off'
-    },
-    settings: {
-      'import/resolver': {
-        typescript: {
-          project: './tsconfig.json'
-        }
-      }
+        { vars: 'all', varsIgnorePattern: '^_', args: 'after-used', argsIgnorePattern: '^_' }
+      ]
     }
   }
-];
+);
