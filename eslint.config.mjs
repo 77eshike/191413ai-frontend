fbd7a0bce1c5135d0b3d788665aa605c1ac3196b
77eshike <77eshike@gmail.com>
2025-07-22 02:52:24 +0800

    chore: fix parser setup in Flat ESLint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,19 @@
 import js from '@eslint/js';
 import prettier from 'eslint-config-prettier';
 import tseslint from '@typescript-eslint/eslint-plugin';
+import parser from '@typescript-eslint/parser';
 
 export default [
   js.configs.recommended,
   {
     files: ['**/*.ts', '**/*.tsx'],
     languageOptions: {
-      parser: '@typescript-eslint/parser',
+      parser,
       parserOptions: {
         sourceType: 'module',
+        ecmaVersion: 2020,
         project: './tsconfig.json',
+        ecmaFeatures: { jsx: true },
       },
     },
     plugins: {
