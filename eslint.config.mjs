7b37bbee8100fc7e06dd37eb9b54bc4a0fd72684
77eshike <77eshike@gmail.com>
2025-07-22 02:50:27 +0800

    chore: migrate ESLint config to Flat Config (ESM)

---
+++ eslint.config.mjs
@@ -0,0 +1,25 @@
+import js from '@eslint/js';
+import prettier from 'eslint-config-prettier';
+import tseslint from '@typescript-eslint/eslint-plugin';
+
+export default [
+  js.configs.recommended,
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    languageOptions: {
+      parser: '@typescript-eslint/parser',
+      parserOptions: {
+        sourceType: 'module',
+        project: './tsconfig.json',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tseslint,
+    },
+    rules: {
+      '@typescript-eslint/no-unused-vars': 'warn',
+      '@typescript-eslint/no-explicit-any': 'off',
+    },
+  },
+  prettier,
+];
